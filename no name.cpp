654f0123e7a259ec158d591e17e5bc330f4124a0
Youssef Ghareb <89851285+yuossefghareb@users.noreply.github.com>
2024-07-21 00:15:56 +0300

    Update no name.cpp

--- no name.cpp
+++ no name.cpp
@@ -14,3 +14,50 @@ for (int q = 0; q < Q; q++)
   cin >> x1 >> y1 >> x2 >> y2;
   cout << prefix[x2][y2] - prefix[x1 - 1][y2] -prefix[x2][y1 - 1] +prefix[x1 - 1][y1 - 1]<< '\n';
 }
+
+
+//  numberOfDivisors  1e18
+ll numberOfDivisors(ll n)
+{
+    int primes[]={2,3,5,7,11,13,17,19};
+    ll num=1,ans=1;
+    for (int it:primes)
+    {
+        int c=0;
+        while(n%it==0)
+        {
+            n/=it;
+            c++;
+        }
+        ans*=c+1;
+        num*=it;
+    }
+    int all=0;
+    for(int i=1;i<num;i++)
+    {
+        bool can=1;
+        for (int it:primes)
+        {
+            if(i%it==0)
+            {
+                can=0;
+            }
+        }
+        if(can)
+        {
+            ll o=i;
+            for (;o*o<n;o+=num)
+            {
+                if(n%o==0)
+                {
+                    all += 2;
+                }
+            }
+            if (o*o==n)
+            {
+                all++;
+            }
+        }
+    }
+    return ans*all;
+}
