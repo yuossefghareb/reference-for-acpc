d3d2e6f48c092a4205e679dce7a7eeb37af33d9e
Youssef Ghareb <89851285+yuossefghareb@users.noreply.github.com>
2024-07-21 00:35:18 +0300

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,51 @@
+
+    Node*root=new Node();
+    void insert(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)
+            {
+                cur->child[idx]=new Node();
+            }
+            cur=cur->child[idx];
+            cur->Prefix++;
+        }
+        cur->IsEnd++;
+    }
+    bool SearchWord(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->IsEnd;
+    }
+    int CountWord(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->IsEnd;
+    }
+    int CountPrefix(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->Prefix;
+    }
+};
